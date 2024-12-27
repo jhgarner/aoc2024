27a930836a61f1920ac5189b391093d0beae0a15
Jack Garner <jacksonhgarner@outlook.com>
2024-12-26 22:00:45 -0800

    Slightly lawful Traversable

--- Utils/Grid.lean
+++ Utils/Grid.lean
@@ -9,38 +9,104 @@ structure SArray (size : Nat) (α : Type) where
 instance [ToString α] : ToString (SArray size α) where
   toString arr := arr.val.foldl (· ++ toString ·) ""
 
+instance : EmptyCollection (SArray 0 α) where
+  emptyCollection := {val := #[], val_is_size := rfl}
+
+instance [Pure m] [EmptyCollection a]: EmptyCollection (m a) where
+  emptyCollection := pure {}
+
+def SArray.map (f: α → β): SArray size α → SArray size β
+| {val, val_is_size} =>
+  let val := val.map f
+  {val, val_is_size := (by simp[val, val_is_size])}
+
+instance : Functor (SArray size) where
+  map := SArray.map
+
+instance : LawfulFunctor (SArray size) where
+  id_map := by
+    simp[Functor.map, SArray.map]
+  map_const := by
+    simp[Functor.map, Functor.mapConst, SArray.map]
+  comp_map := by
+    simp[Functor.map, SArray.map]
+
 def SArray.replicate (a: α) : SArray size α :=
   {val := Array.mk $ List.replicate size a, val_is_size := by simp}
 
 def SArray.modify (arr: SArray size α) (i: Nat) (f: α → α) : SArray size α :=
   {val := arr.val.modify i f, val_is_size := by simp[arr.val_is_size]}
 
-instance : Functor (SArray size) where
-  map f 
-    | {val, val_is_size} =>
-      let val := val.map f
-      {val, val_is_size := (by simp[val, val_is_size])}
+def SArray.push (arr: SArray i α) (a: α): SArray (i+1) α :=
+  { val := arr.val.push a
+  , val_is_size := by simp[Array.size_push, arr.val_is_size]
+  }
 
-def SArray.traverseHelper [Monad m] (i_le_size: i ≤ size) (f: α → m β) (input: SArray size α) (output: SArray i β) : m (SArray size β) := do
-  have := output.val_is_size
-  have := input.val_is_size
+protected def SArray.traverseHelper
+  [Applicative m]
+  (i_le_size: i ≤ size)
+  (f: α → m β)
+  (input: SArray size α)
+  (output: m (SArray i β))
+  : m (SArray size β) :=
   if p : size = i
-    then pure {val := output.val, val_is_size := by simp[*]}
+    then output <&> fun output => {output with val_is_size := by simp[output.val_is_size, *]}
     else
-      have: i < size := Nat.lt_of_le_of_ne i_le_size (p ∘ Eq.symm)
-      f (input.val[i]) >>= fun b =>
-        let pushed := {val := output.val.push b, val_is_size := by simp[Array.size_push, *]}
-        SArray.traverseHelper (by exact this) f input pushed
+      have := input.val_is_size
+      have i_lt_size: i < size := Nat.lt_of_le_of_ne i_le_size (p ∘ Eq.symm)
+      let output := push <$> output <*> f (input.val[i])
+      SArray.traverseHelper i_lt_size f input output
 
-def SArray.traverse [Monad m] (f: α → m β) (arr: SArray size α) : m (SArray size β) := do
-  arr.traverseHelper (Nat.zero_le size) f {val := #[], val_is_size := rfl}
+def SArray.traverse [Applicative m] (f: α → m β) (arr: SArray size α) : m (SArray size β) :=
+  arr.traverseHelper (Nat.zero_le size) f {}
 
+instance : Traversable (SArray size) where
+  traverse := SArray.traverse
 
-def SArray.fin_sarray_size (arr : SArray size α) : arr.val.size = size := by
-  rw[arr.val_is_size]
+theorem empty_arr (s: size = 0) (arr: SArray size α): arr = {val := #[], val_is_size := by simp[*]} := by
+  induction arr
+  simp[Array.size_empty]
+  case mk val val_is_size =>
+    rewrite [s] at val_is_size
+    exact Array.eq_empty_of_size_eq_zero val_is_size
+
+theorem arr_take_size (arr: Array α): arr.take arr.size = arr := by
+  simp[Array.take, Array.take.loop]
+
+theorem traverseHelper_id
+  (arr: SArray size α)
+  (ol: output.val = arr.val.take i):
+  SArray.traverseHelper (i := i) (m := Id) i_le_size (fun x => x) arr output = arr := by
+    rw[SArray.traverseHelper]
+    split
+    case isTrue th =>
+      -- Simplify ol to say that arr = output
+      simp[<- th, <- arr.val_is_size, arr_take_size arr.val] at ol
+      -- Becase the base case just returns output, we're good
+      simp[*]
+    case isFalse th => 
+      exact traverseHelper_id arr $ by
+        simp[*, Seq.seq, SArray.push]
+        -- Prove that the arrays are equal because their lists are equal
+        exact Array.ext' $ by
+          have lt := (Nat.lt_or_eq_of_le i_le_size).resolve_right (th ∘ Eq.symm)
+          rw[<- arr.val_is_size, Array.size_eq_length_toList] at lt
+          simp[*, List.take_add_one, List.getElem?_eq_getElem lt]
+          rfl
+
+instance : LawfulTraversable (SArray size) where
+  id_traverse arr := by
+    simp[Traversable.traverse, SArray.traverse, Pure.pure, EmptyCollection.emptyCollection]
+    -- This sorry seemms like it should be easy to prove
+    exact traverseHelper_id arr (by simp[*]; sorry)
+  comp_traverse := by
+    simp[Traversable.traverse, SArray.traverse, Functor.map, Functor.Comp.mk, *]
+    sorry
+  traverse_eq_map_id := sorry
+  naturality := sorry
 
 instance : GetElem (SArray size α) (Fin size) α (fun _ _ => true) where
-  getElem arr i _ := arr.val[i]'(by simp[arr.fin_sarray_size])
+  getElem arr i _ := arr.val[i]'(by simp[arr.val_is_size])
 
 def SArray.set (i: Fin size) (a : α) : SArray size α → SArray size α
   | {val, val_is_size} =>
@@ -54,8 +120,8 @@ def tryingSize (size : Nat) (arr: Array α) : Option $ SArray size α :=
 def SArray.of (arr: Array α) : SArray arr.size α := ⟨arr, rfl⟩
 
 def SArray.mapFinIdx (arr : SArray size α) (f : Fin size → α → β) : SArray size β :=
-  { val := arr.val.mapFinIdx fun n a => f (arr.fin_sarray_size ▸ n) a
-  , val_is_size := by rw[Array.size_mapFinIdx]; exact arr.fin_sarray_size
+  { val := arr.val.mapFinIdx fun n a => f (arr.val_is_size ▸ n) a
+  , val_is_size := by simp[Array.size_mapFinIdx, arr.val_is_size]
   }
 
 def SArray.findSomeIdx? (arr: SArray size α) (f: Fin size → α → Option β) : Option β :=
